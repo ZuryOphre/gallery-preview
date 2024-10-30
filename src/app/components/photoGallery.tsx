a44a5fccc5cdc30abefdae5e117b0b1443d59614
ZuryOphre <zury.martinez.saldivia@gmail.com>
2024-10-30 11:54:11 -0300

    vs7

--- src/app/components/photoGallery.tsx
+++ src/app/components/photoGallery.tsx
@@ -1,12 +1,13 @@
-"use client";
+'use client';
+
 
 import React, { useEffect, useState } from 'react';
 import { ref, onValue } from 'firebase/database';
 import { database, storage } from '@/app/pages/config/firebase';
 import { getDownloadURL, ref as storageRef } from 'firebase/storage';
 import { motion } from 'framer-motion';
-import Lightbox from 'yet-another-react-lightbox';
-import 'yet-another-react-lightbox/styles.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 type Photo = {
   id: string;
@@ -15,8 +16,6 @@ type Photo = {
 
 const PhotoGallery: React.FC = () => {
   const [photos, setPhotos] = useState<Photo[]>([]);
-  const [isOpen, setIsOpen] = useState(false);
-  const [photoIndex, setPhotoIndex] = useState(0);
 
   useEffect(() => {
     const dbRef = ref(database, 'photos');
@@ -36,37 +35,29 @@ const PhotoGallery: React.FC = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-      {photos.map((photo, index) => (
-        <motion.div
-          key={photo.id}
-          className="cursor-pointer w-full h-48 md:h-40 lg:h-32"
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          transition={{ duration: 0.3 }}
-          onClick={() => {
-            setIsOpen(true);
-            setPhotoIndex(index);
-          }}
-        >
-          <img
-            src={photo.url}
-            alt=""
-            className="w-full h-full max-h-[500px] max-w-[500px] object-contain rounded-lg shadow-md"
-          />
-        </motion.div>
-      ))}
-      {isOpen && (
-        <Lightbox
-          open={isOpen}
-          close={() => setIsOpen(false)}
-          slides={photos.map((photo) => ({ src: photo.url }))}
-          currentIndex={photoIndex}
-          onIndexChange={setPhotoIndex}
-        />
-      )}
-    </div>
+    <PhotoProvider>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+        {photos.map((photo) => (
+          <motion.div
+            key={photo.id}
+            className="cursor-pointer w-full h-48 md:h-40 lg:h-32"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ duration: 0.3 }}
+          >
+            <PhotoView src={photo.url}>
+              <img
+                src={photo.url}
+                alt=""
+                className="w-full h-full max-h-[500px] max-w-[500px] object-contain rounded-lg shadow-md"
+              />
+            </PhotoView>
+          </motion.div>
+        ))}
+      </div>
+    </PhotoProvider>
   );
 };
 
 export default PhotoGallery;
+
