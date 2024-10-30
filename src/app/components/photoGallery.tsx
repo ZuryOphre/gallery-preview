103570696f971a19dbefea94ea67de3e47e32530
ZuryOphre <zury.martinez.saldivia@gmail.com>
2024-10-30 11:27:52 -0300

    vs4

--- src/app/components/photoGallery.tsx
+++ src/app/components/photoGallery.tsx
@@ -17,47 +17,48 @@ type Photo = {
   url: string;
 };
 
+
 const PhotoGallery: React.FC = () => {
-  const [photos, setPhotos] = useState<Photo[]>([]);
-
-  useEffect(() => {
-    const dbRef = ref(database, 'photos');
-    onValue(dbRef, (snapshot) => {
-      const data = snapshot.val();
-      if (data) {
-        const photosArray = Object.keys(data).map(async (key) => {
-          const url = await getDownloadURL(storageRef(storage, data[key].storagePath));
-          return {
-            id: key,
-            url,
-          };
-        });
-        Promise.all(photosArray).then(setPhotos);
-      }
-    });
-  }, []);
-
-  return (
-    <LightGallery plugins={[lgThumbnail]} elementClassNames="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-      {photos.map((photo) => (
-        <motion.div
-          key={photo.id}
-          className="cursor-pointer w-full h-48 md:h-40 lg:h-32"
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          transition={{ duration: 0.3 }}
-        >
-          <a href={photo.url}>
-            <img
-              src={photo.url}
-              alt=""
-              className="w-full h-full object-cover rounded-lg shadow-md"
-            />
-          </a>
-        </motion.div>
-      ))}
-    </LightGallery>
-  );
-};
-
-export default PhotoGallery;
+    const [photos, setPhotos] = useState<Photo[]>([]);
+  
+    useEffect(() => {
+      const dbRef = ref(database, 'photos');
+      onValue(dbRef, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          const photosArray = Object.keys(data).map(async (key) => {
+            const url = await getDownloadURL(storageRef(storage, data[key].storagePath));
+            return {
+              id: key,
+              url,
+            };
+          });
+          Promise.all(photosArray).then(setPhotos);
+        }
+      });
+    }, []);
+  
+    return (
+      <LightGallery plugins={[lgThumbnail]} elementClassNames="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+        {photos.map((photo) => (
+          <motion.div
+            key={photo.id}
+            className="cursor-pointer w-full h-48 md:h-40 lg:h-32"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ duration: 0.3 }}
+          >
+            <a href={photo.url}>
+              <img
+                src={photo.url}
+                alt=""
+                className="w-full h-full max-h-[500px] max-w-[500px] object-contain rounded-lg shadow-md" // Limitando el tamaño de la imagen
+              />
+            </a>
+          </motion.div>
+        ))}
+      </LightGallery>
+    );
+  };
+  
+  export default PhotoGallery;
