b03e2c7364882603a2798c48be6f9ae0bcf3a709
ZuryOphre <zury.martinez.saldivia@gmail.com>
2024-10-30 11:36:00 -0300

    vs5

--- src/app/components/photoGallery.tsx
+++ src/app/components/photoGallery.tsx
@@ -1,64 +1,72 @@
-
-// components/PhotoGallery.tsx
 "use client";
 
 import React, { useEffect, useState } from 'react';
 import { ref, onValue } from 'firebase/database';
 import { database, storage } from '@/app/pages/config/firebase';
 import { getDownloadURL, ref as storageRef } from 'firebase/storage';
-import LightGallery from 'lightgallery/react';
-import 'lightgallery/css/lightgallery.css';
-import 'lightgallery/css/lg-thumbnail.css';
-import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import { motion } from 'framer-motion';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
 
 type Photo = {
   id: string;
   url: string;
 };
 
-
 const PhotoGallery: React.FC = () => {
-    const [photos, setPhotos] = useState<Photo[]>([]);
-  
-    useEffect(() => {
-      const dbRef = ref(database, 'photos');
-      onValue(dbRef, (snapshot) => {
-        const data = snapshot.val();
-        if (data) {
-          const photosArray = Object.keys(data).map(async (key) => {
-            const url = await getDownloadURL(storageRef(storage, data[key].storagePath));
-            return {
-              id: key,
-              url,
-            };
-          });
-          Promise.all(photosArray).then(setPhotos);
-        }
-      });
-    }, []);
-  
-    return (
-      <LightGallery plugins={[lgThumbnail]} elementClassNames="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-        {photos.map((photo) => (
-          <motion.div
-            key={photo.id}
-            className="cursor-pointer w-full h-48 md:h-40 lg:h-32"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            transition={{ duration: 0.3 }}
-          >
-            <a href={photo.url}>
-              <img
-                src={photo.url}
-                alt=""
-                className="w-full h-full max-h-[500px] max-w-[500px] object-contain rounded-lg shadow-md" // Limitando el tamaño de la imagen
-              />
-            </a>
-          </motion.div>
-        ))}
-      </LightGallery>
-    );
-  };
-  
-  export default PhotoGallery;
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
+
+  useEffect(() => {
+    const dbRef = ref(database, 'photos');
+    onValue(dbRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const photosArray = Object.keys(data).map(async (key) => {
+          const url = await getDownloadURL(storageRef(storage, data[key].storagePath));
+          return {
+            id: key,
+            url,
+          };
+        });
+        Promise.all(photosArray).then(setPhotos);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+      {photos.map((photo, index) => (
+        <motion.div
+          key={photo.id}
+          className="cursor-pointer w-full h-48 md:h-40 lg:h-32"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          transition={{ duration: 0.3 }}
+          onClick={() => {
+            setIsOpen(true);
+            setPhotoIndex(index);
+          }}
+        >
+          <img
+            src={photo.url}
+            alt=""
+            className="w-full h-full max-h-[500px] max-w-[500px] object-contain rounded-lg shadow-md"
+          />
+        </motion.div>
+      ))}
+      {isOpen && (
+        <Lightbox
+          open={isOpen}
+          close={() => setIsOpen(false)}
+          slides={photos.map((photo) => ({ src: photo.url }))}
+          currentIndex={photoIndex}
+          onIndexChange={setPhotoIndex}
+        />
+      )}
+    </div>
+  );
+};
+
+export default PhotoGallery;
